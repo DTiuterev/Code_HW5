f9edba8ed99992e0ed836611b2a74c4b8f68d353
Dmitrii <dtiuterev@rambler.ru>
2022-09-02 13:44:42 +0300

    added Ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Массив случайных положительных трехзначных чисел. Подсчет количества четных чисел в массиве.
+Console.WriteLine("Введите количество элементов массива: ");
+int q = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сейчас я заполню его случайными трехзачными положительными числами и посчитаю количество четных элементов массива ");
+
+int[] array = new int[q];
+
+int Method (int a)
+{
+        return array[a];
+}
+ 
+    Console.WriteLine("Ваш массив:");
+    
+for (int i = 0; i < q; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(" " + Method(i) + " ");
+}
+System.Console.WriteLine();
+
+int quanIndPos = 0;
+
+for(int j = 0; j < q; j++)
+{
+    if(array[j] % 2 == 0)
+        {
+            quanIndPos ++;
+        } 
+}
+Console.WriteLine($"Количество четных элементов масcива = {quanIndPos}");
+
+
