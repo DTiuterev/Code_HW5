1fc81a6492b086f1358cacfd1c7b39457b32111c
Dmitrii <dtiuterev@rambler.ru>
2022-09-08 19:38:41 +0300

    Added Ex38

---
+++ Ex38/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+Console.WriteLine("Задайте произвольный массив, указав количество элементов и вводя последовательно элементы массива");
+Console.Write("Ведите количество элементов массива:\t");
+
+void DiffMaxMin()
+{
+int elementsCount = int.Parse(Console.ReadLine());
+double[] array = new double[elementsCount];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива с индексом {i}:\t");
+    array[i] = double.Parse(Console.ReadLine());   
+}
+Console.WriteLine($"Ваш массив: {String.Join(", ", array)}");
+
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }   
+    }
+double diffMaxMin = max - min;
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {diffMaxMin}");
+}
+DiffMaxMin();
